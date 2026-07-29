1705a625b62817daff7617846653d4ef4ee08d08
sohee6989 <144779368+sohee6989@users.noreply.github.com>
2026-07-29 17:26:12 +0900

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.py
@@ -0,0 +1,50 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        start = 1
+        end = max(piles)
+        isStart = False
+        isEnd = False
+
+        while start < end:
+            if isPossible(start, h, piles) :
+                isStart = True
+            else:
+                isStart = False
+            
+            if isPossible(end, h, piles):
+                isEnd = True
+            else:
+                isEnd = False
+
+            if isStart:
+                return start
+
+            if end - start == 1:
+                return end
+        
+            half_index = start + (end - start) // 2
+
+            isHalf = isPossible(half_index, h, piles)
+
+            if not (isHalf ^ isStart):
+                start = half_index
+            else:
+                end = half_index
+        
+
+    
+def isPossible(rate: int, h: int, piles: List[int]) -> bool:
+    hours = 0
+
+    for p in piles:
+        if (p % rate == 0):
+            hours += p // rate
+        else:
+            hours += (p // rate + 1)
+        
+    if hours <= h:
+        return True
+    else:
+        return False
+
+        
