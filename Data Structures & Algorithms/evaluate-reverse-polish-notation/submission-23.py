696035cdbb1570fd30c83dae972fb89066f875f2
sohee6989 <144779368+sohee6989@users.noreply.github.com>
2026-08-11 14:26:42 +0900

    Add: evaluate-reverse-polish-notation - submission-23

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-23.py
@@ -0,0 +1,35 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        num_array = []
+
+        if len(tokens) == 1:
+            return int(tokens[0])
+
+        for token in tokens:
+            # digit은 음수 기호(-)나 소수점(.)이 있으면 False를 반환
+            if token.isdigit(): 
+                token = int(token)
+                num_array.append(token)
+            elif token.startswith('-') and token[1:].isdigit():
+                num_array.append(-int(token[1:]))
+            else:
+                second_num = num_array.pop()
+                first_num = num_array.pop()
+                result = calculate(first_num, second_num, token)
+                num_array.append(result)
+        
+        return result
+    
+def calculate(num1: int, num2: int, op: str):
+    result = 0
+
+    if op == '+':
+        result = num1 + num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '-':
+        result = num1 - num2
+    else:
+        result = int(num1 / num2)
+        
+    return result
